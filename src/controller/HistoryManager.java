7d7993deb55564dd5509f9b3fc6477e79533cfac
Ilya <skrontex@gmail.com>
2025-03-10 22:19:13 +0300

    Работа по 6 спринту:  -изменение работы истории, через двусвязный список.  -история без ограничения и повторов.

--- src/controller/HistoryManager.java
+++ src/controller/HistoryManager.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public interface HistoryManager {
     void add(Task task);                    //добавление просмотренной задачи в историю
+    void remove(int id);                    //удаление задачи из истории
     List<Task> getHistory();                //вывод списка истории
 }
